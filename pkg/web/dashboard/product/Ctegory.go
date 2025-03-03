68a46520492cf9f09992fc1424ea8155878d3656
moslem ghalandari <moslem@moslems-MacBook-Air.local>
2025-03-03 23:16:25 +0330

    go produt

--- pkg/web/dashboard/product/Ctegory.go
+++ pkg/web/dashboard/product/Ctegory.go
@@ -1,7 +1,39 @@
 package product
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"nextgen/internals/gintemplrenderer"
+	"nextgen/pkg/web/dashboard/product"
+	"nextgen/templates/dashboard/dashboardcomponents"
+	"sort"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Category(c *gin.Context) {
+	categoryPagePros := product.CategoryPagePros{}
+	layoutProp, exists := c.Get("LayoutProp")
+
+	if !exists {
+		layoutProp = dashboardcomponents.LayoutProp{}
+	}
+
+	categoryPagePros.LayoutProp = layoutProp.(dashboardcomponents.LayoutProp)
+
+	productPageHeaderProps := make([]product.ProductPageHeaderProp, 0)
+	for key, value := range ProductPageHeaderProps {
+		if key == "ProductsPage" {
+			value.Class = currentHeaderElementClass
+		} else {
+			value.Class = headerElementsClass
+		}
+		productPageHeaderProps = append(productPageHeaderProps, *value)
+	}
+	sort.Slice(productPageHeaderProps, func(i, j int) bool {
+		return productPageHeaderProps[i].Url > productPageHeaderProps[j].Url
+	})
+
+	r := gintemplrenderer.New(c.Request.Context(), http.StatusOK, product.CategoryPage(categoryPagePros))
+	c.Render(http.StatusOK, r)
 
 }
